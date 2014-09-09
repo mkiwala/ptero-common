5b27e5b0d064d04ef8996a8a433d15446c2ff204
David Morton <davidlmorton@gmail.com>
2014-09-09 13:26:28 -0500

    Update response returned when authorization is missing

--- ptero_common/auth.py
+++ ptero_common/auth.py
@@ -1,19 +1,38 @@
 from flask import request, Response
+import re
+
+class ProtectedEndpoint(object):
+    def __init__(self, scopes=[], claims=[], audiences=[]):
+        self.scopes = scopes
+        self.claims = claims
+        self.audiences = audiences
+
+    def __call__(self, target):
+        def handle_request(*args, **kwargs):
+            response, id_token = _parse_request(request, self.scopes,
+                    self.claims, self.audiences)
+            if (response):
+                return response
+            else:
+                return target(*args, id_token=id_token, **kwargs)
+        return handle_request
+
+protected_endpoint = ProtectedEndpoint
 
 
-def protected_endpoint(target):
-    def handle_request(*args, **kwargs):
-        response, id_token = _parse_request(request)
-        if (response):
-            return response
-        else:
-            return target(*args, id_token=id_token, **kwargs)
-    return handle_request
-
-def _parse_request(request):
+def _parse_request(request, scopes, claims, audiences):
     if ('Authorization' not in request.headers or
             'Identity' not in request.headers):
         return Response(status=401,
-                headers={'WWW-Authenticate': 'Bearer realm="PTero"'}), None
+                headers={'WWW-Authenticate': authenticate_value_text(scopes),
+                         'Identify': identify_value_text(claims, audiences)}), None
     else:
         return None, None
+
+
+def authenticate_value_text(scopes):
+    return 'Bearer realm="PTero", scope="%s"' % ' '.join(scopes)
+
+def identify_value_text(claims, audiences):
+    return 'ID Token claims="%s", aud="%s"' % (', '.join(claims),
+            ', '.join(audiences))
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,7 +5,7 @@ from ptero_common.auth import protected_endpoint
 
 app = flask.Flask('test_app')
 
-@protected_endpoint
+@protected_endpoint(scopes=['a','b'], claims=['c','d'], audiences=['e','f'])
 def target(*args, **kwargs):
     return 'test_return_value'
 
@@ -17,13 +17,29 @@ class TestRequestParser(TestCase):
             return_value = target()
             self.assertEqual(return_value.status, '401 UNAUTHORIZED')
 
+    def test_request_with_no_authorization_returns_authenticate_headers(self):
+        with app.test_request_context(headers=None):
+            return_value = target()
+            self.assertTrue('WWW-Authenticate' in return_value.headers)
+
+            self.assertEqual(return_value.headers['WWW-Authenticate'],
+                    'Bearer realm="PTero", scope="a b"')
+
+    def test_request_with_no_authorization_returns_identify_headers(self):
+        with app.test_request_context(headers=None):
+            return_value = target()
+            self.assertTrue('Identify' in return_value.headers)
+
+            self.assertEqual(return_value.headers['Identify'],
+                    'ID Token claims="c, d", aud="e, f"')
+
     def test_request_with_authorization_returns_targets_result(self):
         with app.test_request_context(headers=VALID_HEADER):
             return_value = target()
             self.assertEqual(return_value, 'test_return_value')
 
     def test_adds_id_token_as_kwarg(self):
-        @protected_endpoint
+        @protected_endpoint()
         def echo_target(*args, **kwargs):
             return (args, kwargs)
 

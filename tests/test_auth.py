2aed84cf960cc0628d0ff12c58f26e23b243e70f
David Morton <davidlmorton@gmail.com>
2014-09-09 14:05:33 -0500

    Shorten test names

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,12 +12,12 @@ def target(*args, **kwargs):
 VALID_HEADER = {'Authorization':'foo', 'Identity':'bar'}
 
 class TestRequestParser(TestCase):
-    def test_request_with_no_authorization_returns_401(self):
+    def test_no_authorization_returns_401(self):
         with app.test_request_context(headers=None):
             return_value = target()
             self.assertEqual(return_value.status, '401 UNAUTHORIZED')
 
-    def test_request_with_no_authorization_returns_authenticate_headers(self):
+    def test_no_authorization_returns_authenticate_headers(self):
         with app.test_request_context(headers=None):
             return_value = target()
             self.assertTrue('WWW-Authenticate' in return_value.headers)
@@ -25,7 +25,7 @@ class TestRequestParser(TestCase):
             self.assertEqual(return_value.headers['WWW-Authenticate'],
                     'Bearer realm="PTero", scope="a b"')
 
-    def test_request_with_no_authorization_returns_identify_headers(self):
+    def test_no_authorization_returns_identify_headers(self):
         with app.test_request_context(headers=None):
             return_value = target()
             self.assertTrue('Identify' in return_value.headers)
@@ -33,7 +33,7 @@ class TestRequestParser(TestCase):
             self.assertEqual(return_value.headers['Identify'],
                     'ID Token claims="c, d", aud="e, f"')
 
-    def test_request_with_authorization_returns_targets_result(self):
+    def test_returns_targets_result(self):
         with app.test_request_context(headers=VALID_HEADER):
             return_value = target()
             self.assertEqual(return_value, 'test_return_value')

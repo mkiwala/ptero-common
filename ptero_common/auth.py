31e0e0bd8d56c2b61fcb0e769913b421f5bc8c41
David Morton <davidlmorton@gmail.com>
2014-09-10 12:41:39 -0500

    Define custom exception class

--- ptero_common/auth.py
+++ ptero_common/auth.py
@@ -2,6 +2,8 @@ from flask import request, Response
 import jot
 import re
 
+class MalformedAccessTokenError(Exception): pass
+
 class ProtectedEndpoint(object):
     def __init__(self, scopes=[], claims=[], audiences=[]):
         self.scopes = scopes
@@ -30,7 +32,7 @@ def _parse_request(request, scopes, claims, audiences):
 
     try:
         access_token = parse_authorization_text(request.headers['Authorization'])
-    except ValueError as e:
+    except MalformedAccessTokenError as e:
         return Response(status=400,
                 headers={'WWW-Authenticate':
                         '%s, error="invalid_request", error_description="The Bearer token is malformed"' %
@@ -58,6 +60,6 @@ def identify_value_text(claims, audiences):
 def parse_authorization_text(text):
     match_object = re.search('Bearer (.*)', text)
     if match_object is None:
-        raise ValueError('Cannot parse authorization_text (%s)' % text)
+        raise MalformedAccessTokenError
     else:
         return match_object.groups()[0]

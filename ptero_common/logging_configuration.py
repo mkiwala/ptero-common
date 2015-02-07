cbc449034a875d6619e5ae7c7fca9e0db78406eb
David Morton <davidlmorton@gmail.com>
2015-02-07 20:26:08 +0000

    Don't try to build Request objects with 'timeout' kwarg
    This kwarg is okay for the requests.get/requests.post/ect functions, but
    not for the Request object constructor.

--- ptero_common/logging_configuration.py
+++ ptero_common/logging_configuration.py
@@ -94,7 +94,12 @@ def _log_request(target, kind):
                 kind.upper(), pformat(args), pformat(kwargs), str(e))
             raise
 
+        if 'timeout' in kwargs:
+            # timout is an argument to requests.get/post/ect but not
+            # Request.__init__
+            del kwargs['timeout']
         r = Request(kind.upper(), *args, **kwargs)
+
         logger.info("%s from %s  %s", response.status_code, kind.upper(), r.url)
         for name in ['params', 'headers', 'data']:
             logger.debug("    %s%s: %s", name[0].upper(), name[1:],

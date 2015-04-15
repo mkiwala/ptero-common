4870406f0747db033809f9f7b592062bda4b3a39
David Morton <davidlmorton@gmail.com>
2015-04-15 03:52:48 +0000

    Add statuses module

---
+++ ptero_common/statuses.py
@@ -0,0 +1,28 @@
+new = 'new'
+scheduled = 'scheduled'
+running = 'running'
+succeeded = 'succeeded'
+failed = 'failed'
+errored = 'errored'
+canceled = 'canceled'
+
+TERMINAL_STATUSES = (succeeded, failed, errored, canceled)
+def is_terminal(status):
+    return status in TERMINAL_STATUSES
+
+VALID_STATUSES = (new, scheduled, running) + TERMINAL_STATUSES
+def is_valid(status):
+    return status in VALID_STATUSES
+
+VALID_STATUS_TRANSITIONS = {
+    new: (scheduled, running, failed, errored, succeeded,
+        canceled),
+    scheduled: (running, failed, errored, succeeded ,canceled),
+    running: (failed, errored, succeeded ,canceled),
+    failed: tuple(),
+    errored: tuple(),
+    succeeded: tuple(),
+    canceled: tuple(),
+}
+def is_valid_transition(old_status, new_status):
+    return new_status in VALID_STATUS_TRANSITIONS[old_status]
